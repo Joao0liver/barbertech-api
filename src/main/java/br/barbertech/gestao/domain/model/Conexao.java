bf8e3a69aa2c992d1547c8d3cefcad190ce74817
Joao0liver <joaoolv.profissional@gmail.com>
2025-10-01 18:26:02 -0300

    feat: Criação da classe de conexão com o banco de dados

---
+++ src/main/java/br/barbertech/gestao/domain/model/Conexao.java
@@ -0,0 +1,32 @@
+package br.barbertech.gestao.domain.model;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Conexao {
+
+    private static final String url = "jdbc:mysql://localhost:3306/bt_database";
+    private static final String user = "root";
+    private static final String pass = "";
+
+    private static Connection conn;
+
+    public static Connection getConexao() throws SQLException {
+
+        try{
+            if(conn == null){
+                conn = DriverManager.getConnection(url, user, pass);
+                return conn;
+            }else{
+                return conn;
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
+
+
+    }
+
+}
