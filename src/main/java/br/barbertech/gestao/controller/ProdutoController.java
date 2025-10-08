c55cb8367250a912d9e677cb6785d451005b5d69
Joao0liver <joaoolv.profissional@gmail.com>
2025-10-08 19:00:30 -0300

    feat: Deletar Produto

--- src/main/java/br/barbertech/gestao/controller/ProdutoController.java
+++ src/main/java/br/barbertech/gestao/controller/ProdutoController.java
@@ -6,10 +6,7 @@ import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -23,16 +20,26 @@ public class ProdutoController {
     private ProdutoRepository repository;
 
     @GetMapping ("/produtos")
-    public List<Produto> listar() {
+    public List<Produto> listarProduto() {
 
         return manager.createQuery("from Produto",Produto.class).getResultList();
 
     }
 
     @PostMapping ("/cadastrar-produto")
-    public ResponseEntity<Produto> cadastrar(@RequestBody Produto novoProduto) {
+    public ResponseEntity<Produto> cadastrarProduto(@RequestBody Produto novoProduto) {
         Produto produtoSalvo = repository.save(novoProduto);
         return ResponseEntity.ok(produtoSalvo);
+    }
+
+    @DeleteMapping ("/produtos/{id_produto}")
+    public ResponseEntity<Void> deletarProduto(@PathVariable Long id_produto) {
+        if (!repository.existsById(id_produto)) {
+            return ResponseEntity.notFound().build();
+        }
+
+        repository.deleteById(id_produto);
+        return ResponseEntity.noContent().build(); // 204
     }
 
 }
