6aac476a018f5ec5b2a1c335b6407d07c9ac77c6
nicolas-davila <nicolasdavilafsantos@gmail.com>
2025-11-05 13:12:00 -0300

    refactor: Ajustes de sintaxe e modelo de mapping para dto.

---
+++ src/main/java/br/barbertech/gestao/dto/ServicoDTO.java
@@ -0,0 +1,5 @@
+package br.barbertech.gestao.dto;
+
+
+public record ServicoDTO(String nomeServico, double precoCusto, double precoVenda, boolean statusServico) {
+}
--- src/main/java/br/barbertech/gestao/entity/Servico.java
+++ src/main/java/br/barbertech/gestao/entity/Servico.java
@@ -97,5 +97,4 @@ public class Servico {
             statusServico = true;
         }
     }
-
 }
