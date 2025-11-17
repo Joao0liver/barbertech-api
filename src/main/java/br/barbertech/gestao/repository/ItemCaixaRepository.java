f008af53b27f83531c43efcbb6469547b20a15c6
Joao0liver <joaoolv.profissional@gmail.com>
2025-11-17 18:36:32 -0300

    feat: Repository da Classe ItemCaixa

---
+++ src/main/java/br/barbertech/gestao/repository/ItemCaixaRepository.java
@@ -0,0 +1,8 @@
+package br.barbertech.gestao.repository;
+
+import br.barbertech.gestao.entity.ItemCaixa;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ItemCaixaRepository extends JpaRepository<ItemCaixa, Long> {
+
+}
