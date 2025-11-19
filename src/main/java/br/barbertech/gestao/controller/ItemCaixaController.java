41ee6397d01ca7eef1b2bd56d20dbf14b88f7e25
Joao0liver <joaoolv.profissional@gmail.com>
2025-11-19 14:59:54 -0300

    feat: CRUD de ItemCaixa

--- src/main/java/br/barbertech/gestao/controller/ItemCaixaController.java
+++ src/main/java/br/barbertech/gestao/controller/ItemCaixaController.java
@@ -1,9 +1,13 @@
 package br.barbertech.gestao.controller;
 
+import br.barbertech.gestao.dto.ItemCaixaDto;
+import br.barbertech.gestao.entity.ItemCaixa;
 import br.barbertech.gestao.repository.ItemCaixaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/itemCaixa")
@@ -11,5 +15,46 @@ public class ItemCaixaController {
 
     @Autowired
     private ItemCaixaRepository repository;
+
+    @GetMapping
+    public List<ItemCaixa> listar() {
+        return repository.findAll();
+    }
+
+    @PostMapping
+    public ResponseEntity<ItemCaixa> cadastrar(@RequestBody ItemCaixaDto dto) {
+        ItemCaixa novoItemCaixa = new ItemCaixa();
+
+        novoItemCaixa.setIdItem(dto.idItem());
+        novoItemCaixa.setQuantidadeItem(dto.quantidadeItem());
+        novoItemCaixa.setValor(dto.valor());
+
+        ItemCaixa itemCaixaSalvo = repository.save(novoItemCaixa);
+        return ResponseEntity.ok(itemCaixaSalvo);
+    }
+
+    @DeleteMapping("/{idItemCaixa}")
+    public ResponseEntity<Void> deletar(@PathVariable Long idItemCaixa) {
+        if (!repository.existsById(idItemCaixa)) {
+            return ResponseEntity.notFound().build();
+        }
+
+        repository.deleteById(idItemCaixa);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PutMapping("/{idItemCaixa}")
+    public ResponseEntity<ItemCaixa> atualizarItemCaixa(@PathVariable Long idItemCaixa, @RequestBody ItemCaixaDto dto) {
+        return repository.findById(idItemCaixa)
+                .map(itemCaixaExistente -> {
+                    itemCaixaExistente.setIdItem(dto.idItem());
+                    itemCaixaExistente.setQuantidadeItem(dto.quantidadeItem());
+                    itemCaixaExistente.setValor(dto.valor());
+
+                    ItemCaixa itemCaixaSalvo = repository.save(itemCaixaExistente);
+                    return ResponseEntity.ok(itemCaixaSalvo);
+                })
+                .orElseGet(() -> ResponseEntity.notFound().build());
+    }
 
 }
